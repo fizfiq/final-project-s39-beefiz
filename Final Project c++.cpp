380be43fbbbc82ea3355434b7d71bbfb0aec4455
KillerBee3012 <33883984+KillerBee3012@users.noreply.github.com>
2018-01-30 09:09:22 +0800

    x siap lagi don't know when.... trololoololololo

---
+++ Final Project c++.cpp
@@ -0,0 +1,434 @@
+//Name Aidil Suffian Bin Abu Bakar
+//Name Muhammad Hafiz Afiq Bin Khalid
+// FINAL PROJECT C++ (SOALAN 1 )
+#include <iostream>
+using namespace std;
+
+
+/*
+
+
+
+void whoareyou (int x , int y)
+
+{
+	
+	
+	
+}
+
+void buatapa (int x , int y)
+
+{
+	
+	
+	
+	
+	
+}
+
+void gun (int x , int y)
+
+{
+	
+	
+	
+	
+}
+
+void priceman(int x, int y)
+
+{
+	
+	
+	
+	
+}
+
+void styleman (int x, int y)
+
+{
+	
+	
+	
+	
+	
+}
+
+
+void weightgun ( int x , int y)
+
+
+{
+	
+	
+	
+	
+}
+
+void buyammo ( int x ,int y)
+
+{
+	
+	
+	
+}
+
+void help  (int x , int y)
+
+{
+	
+	
+	
+	
+}
+
+void tips (int x , int y)
+
+{
+	
+	
+	
+}
+
+void bayarlaa (int )
+
+{
+	
+	
+}
+
+
+
+*/
+int bahasa,lesen,option,TODO;
+
+int main ()
+
+
+
+
+{
+	
+	cout<< "Welcome User To Our Gun Shop. Please Select Your Langauge to Understance / Selamat Datang Ke kedai Senjata Kami. Sila Pilih bahasa Anda Untuk Paham Bahasa"<<endl<<endl;
+	
+	cout<< "\t 1. English "<<endl ;                               // Pilih Bahasa first step
+	cout<< "\t 2. Bahasa Melayu "<<endl<<endl;
+	cin >> bahasa;
+	
+	if (bahasa == 1)
+	
+	{
+		
+		cout<<"Do You Guys Have Gun License ? "<<endl;
+		cout<< "\t 1. Yes "<<endl ;                               // Tanya Pasai Lesen Ada ke x?
+	    cout<< "\t 2. No "<<endl<<endl;
+		cin>> lesen;
+		
+		if (lesen == 1)
+		
+		{
+			cout<<"Good ! Welcome Once again !"<<endl ;
+			
+			system ("pause");                                     // Benda yg pause system
+			
+			system ("CLS");                                      //Benda yg clear screen 
+			
+			
+		
+			
+			
+			cout<< "\t 1. First Time ? Need Help ? \t\t 2. Buy Guns \t\t 3. Price List Of Guns "<<endl<<endl;                    // Kat perlu Ada while Loop kalau user letak error
+			cout<< "\t\t\t\t 4. ammo \t\t 5.Need Support ? "<<endl;
+			cin>> option;
+			
+			if (option == 1)
+			
+			{
+			   cout<< "Owh This Is Your First Time... What Do You Want to Do?"<<endl;
+			   cout<< "1.Start a war ?" <<endl;
+			   cout<< "2. Defence Your Country?"<<endl;
+			   cout<< " 3. Feeling Like Heavy ?"<<endl;
+			   cout<< "Feeling like Sneaky ?"<<endl;
+			   
+			   cin>> TODO;
+			   
+			   if (TODO == 1)
+			   
+			   {
+			   	cout<< "Owh You want to start a war.haha nice"<<endl;
+			   	
+				   cout<< "Berat atau Ringan"<<endl;
+				   cin>> gunweight;
+				   
+				   if (gunweight == 1)
+				   {
+				   	  cout<< "mahai atau murah"<<endl;
+				   	  cin>> pricetag;
+				   	  
+				   	  if (pricetag == 1)
+				   	  
+				   	  {
+				   	  	
+				   	  	
+				   	  	
+				      }
+				   	  
+				   	
+				   }
+				   
+				   
+				   
+				   
+				   
+				   
+				  
+			   	cout<< ""
+				   
+				   cout << "What Kind Of Weapon Do You Wish To Have Here"<<endl;
+				   
+				   
+				   
+				   
+				   
+				   
+				   
+				   
+				   cout<< "Here Is The List Kind Of Weapon To : START A WAR "<<endl;
+			   	
+			   	
+			   	
+			   	
+			   	
+			   	
+			   	
+			   	
+			   	
+			   	
+			   	
+			   	
+			   	
+			   }
+			   
+			   else if (TODO == 2)
+			   
+			   {
+			   	cout<< "Defence Your Country That Good . Respect From Us "<<endl;
+			   }
+			   
+			   else if (TODO == 3)
+			   
+			   {
+			   	cout<< "Feeling Heavy Eh"<<endl;
+			   }
+			   
+			   else if (TODO == 4)
+			   
+			   {
+			   	cout<< "Feeling Sneaky Like a Ghost eh i See"<<endl;
+			   	
+			   }
+  
+			   
+			}
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			else if (option == 2)
+			
+			{
+				cout<< "2"	;
+			}
+			
+			else if (option == 3)
+			
+			{
+				cout<< "3"	;
+			}
+			
+			else if (option == 4)
+			
+			{
+				cout<< "4"	;
+			}
+			
+			else if (option == 5)
+			
+			{
+				cout<< "5"	;
+			}
+			
+		
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+		}
+		
+		else
+		
+		{
+			cout<< "Get Lost We Are NOT Selling You Guys Nothing !";
+			
+			
+		}
+		
+		
+		
+		
+	}
+	
+	else if (bahasa == 2)
+	
+	{
+		
+		cout<< "saya melayu";
+	}
+	
+	else
+	
+	{
+		cout<< "vnsbvibsvuibsiv";
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
